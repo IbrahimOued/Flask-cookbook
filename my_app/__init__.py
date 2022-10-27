bc62efe53f18325979ae5a937b6de45665974500
Ibrahim Ouedraogo <brahim.oued@gmail.com>
2022-10-27 13:40:58 +0000

    use of blueprints to modular web app

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -1,5 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-# from my_app.hello.views import *
-import my_app.hello.views
+from flask import Flask 
+from my_app.hello.views import hello 
+ 
+app = Flask(__name__) 
+app.register_blueprint(hello) 
