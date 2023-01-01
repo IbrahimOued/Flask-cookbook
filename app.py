ef081394bdc5fa40270daccd91e52888e9af53c7
Ibrahim Ouedraogo <brahim.oued@gmail.com>
2023-01-01 15:40:56 +0000

    add momentjs class for date printing

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 from flask import Flask
+
+from my_app.momentjs import momentjs
+
+
 app = Flask(__name__)
+
+# set jinja template global
+app.jinja_env.globals['momentjs'] = momentjs
 
 @app.route("/")
 def hello_world():
